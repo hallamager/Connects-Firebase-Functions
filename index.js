5b831edf0f9ff2e7e2a82cf398cf956466175548
hallamager <HallamAger@Hallams-MacBook-Pro.local>
2018-03-13 15:32:09 +0000

    Update

--- index.js
+++ index.js
@@ -4,40 +4,91 @@ admin.initializeApp(functions.config().firebase);
 
 
 
-exports.businessLiked = functions.database.ref('/businessesLiked/{bid}/{uid}').onWrite((event) => {
+exports.sendLikedNotification = functions.database.ref('/businessesLiked/{bid}/{uid}').onWrite((event) => {
 
     const uid = event.params.uid;
     const bid = event.params.bid;
 
-
-
-
     console.log(`${uid} liked ${bid}`);
 
-    return true;
+    // Get the list of device notification tokens.
+    const getDeviceTokensPromise = admin.database().ref(`/business/${bid}/FCM Token`).once('value');
 
-});
+    // Get liked profile
+    const getLikedProfilePromise = admin.auth().getUser(uid);
 
-exports.sendPushNotifcations = functions.database.ref('/businessesLiked/{bid}/{uid}').onWrite(event => {
+    return Promise.all([getDeviceTokensPromise, getLikedProfilePromise]).then((results) => {
+        const tokensSnapshot = results[0];
+        const liker = results[1];
 
-    const payload = {
-
-        notification: {
-
-            title: 'New Message',
-            body: 'Have a look',
-            badge: '1',
-            sound: 'default',
-
+        // Check if there are any device tokens.
+        if (!tokensSnapshot.hasChildren()) {
+            return console.log('There are no notification tokens to send to.');
         }
 
-    };
-    return admin.database().ref('fcmToken').once('value').then(allToken => {
-        if (allToken.val()) {
-            const token = Object.keys(allToken.val());
-            return admin.messaging().sendToDevice(token, payload).then(response => {
+        console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
+        console.log('Fetched liker profile', liker);
 
-            });
+        // Notification details
+        const payload = {
+            notification: {
+
+                title: 'Someone liked your profile',
+                body: `${liker.Username} is now following you.`,
+                badge: '1',
+
+            },
+
         };
+
+        // Listing all tokens.
+        const tokens = Object.keys(tokensSnapshot.val());
+
+        // Send notifications to all tokens.
+        return admin.messaging().sendToDevice(tokens, payload);
+
+    }).then((response) => {
+        // For each message check if there was an error.
+        const tokensToRemove = [];
+        response.results.forEach((results, index) => {
+            const error = result.error;
+            if (error) {
+                console.error('Failure sending notification to', tokens[index], error);
+                // Cleanup the tokens who are not registered anymore
+                if (error.code === 'messaging/invalid-registration-token' || error.code === 'messaging/registration-token-not-valid') {
+                    tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
+                }
+            }
+        });
+        return Promise.all(tokensToRemove)
     });
+
 });
+
+
+
+
+
+//exports.sendPushNotifcations = functions.database.ref('/businessesLiked/{bid}/{uid}').onWrite(event => {
+//
+//    const payload = {
+//
+//        notification: {
+//
+//            title: 'New Message',
+//            body: 'Have a look',
+//            badge: '1',
+//            sound: 'default',
+//
+//        }
+//
+//    };
+//    return admin.database().ref('/businessesLiked/{bid}').once('value').then(allToken => {
+//        if (allToken.val()) {
+//            const token = Object.keys(allToken.val());
+//            return admin.messaging().sendToDevice(token, payload).then(response => {
+//
+//            });
+//        };
+//    });
+//});
