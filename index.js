ef596b8c15a0d38f43b566522188aab6a6104a89
hallamager <HallamAger@Hallams-MacBook-Pro.local>
2018-03-15 14:09:08 +0000

    Matched notification

--- index.js
+++ index.js
@@ -33,8 +33,7 @@ exports.sendLikedNotification = functions.database.ref('/businessesLiked/{bid}/{
         const payload = {
             notification: {
 
-                title: 'Someone liked your profile',
-                body: `${liker.Username} is now following you.`,
+                body: 'Congratulations! Someone liked your profile',
                 badge: '1',
 
             },
@@ -67,28 +66,62 @@ exports.sendLikedNotification = functions.database.ref('/businessesLiked/{bid}/{
 
 
 
+exports.sendMatchedNotification = functions.database.ref('/matches/{uid}/{bid}').onWrite((event) => {
 
+    const uid = event.params.uid;
+    const bid = event.params.bid;
 
-//exports.sendPushNotifcations = functions.database.ref('/businessesLiked/{bid}/{uid}').onWrite(event => {
-//
-//    const payload = {
-//
-//        notification: {
-//
-//            title: 'New Message',
-//            body: 'Have a look',
-//            badge: '1',
-//            sound: 'default',
-//
-//        }
-//
-//    };
-//    return admin.database().ref('/businessesLiked/{bid}').once('value').then(allToken => {
-//        if (allToken.val()) {
-//            const token = Object.keys(allToken.val());
-//            return admin.messaging().sendToDevice(token, payload).then(response => {
-//
-//            });
-//        };
-//    });
-//});
+    console.log(`${bid} matched ${uid}`);
+
+    // Get the list of device notification tokens.
+    const getDeviceTokensPromise = admin.database().ref(`/student/${uid}/FCM Token`).once('value');
+
+    // Get liked profile
+    const getMatchedProfilePromise = admin.auth().getUser(bid);
+
+    return Promise.all([getDeviceTokensPromise, getMatchedProfilePromise]).then((results) => {
+        const tokensSnapshot = results[0];
+        const matcher = results[1];
+
+        // Check if there are any device tokens.
+        if (!tokensSnapshot.hasChildren()) {
+            return console.log('There are no notification tokens to send to.');
+        }
+
+        console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
+        console.log('Fetched matcher profile', matcher);
+
+        // Notification details
+        const payload = {
+            notification: {
+
+                body: 'Congratulations! You have a new match',
+                badge: '1',
+
+            },
+
+        };
+
+        // Listing all tokens.
+        const tokens = Object.keys(tokensSnapshot.val());
+
+        // Send notifications to all tokens.
+        return admin.messaging().sendToDevice(tokens, payload);
+
+    }).then((response) => {
+        // For each message check if there was an error.
+        const tokensToRemove = [];
+        response.results.forEach((results, index) => {
+            const error = result.error;
+            if (error) {
+                console.error('Failure sending notification to', tokens[index], error);
+                // Cleanup the tokens who are not registered anymore
+                if (error.code === 'messaging/invalid-registration-token' || error.code === 'messaging/registration-token-not-valid') {
+                    tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
+                }
+            }
+        });
+        return Promise.all(tokensToRemove)
+    });
+
+});
