608de3738b28f217957ef83d3ad0e3469473bfeb
hallamager <HallamAger@Hallams-MacBook-Pro.local>
2018-04-30 22:44:04 +0100

    sendInviteResponseNotification

--- index.js
+++ index.js
@@ -4,6 +4,68 @@ admin.initializeApp(functions.config().firebase);
 
 
 
+exports.sendInviteResponseNotification = functions.database.ref('/organisedChats/{uid}/{bid}/Response').onUpdate((event) => {
+
+    const uid = event.params.uid;
+    const bid = event.params.bid;
+
+    console.log(`${uid} responsed to ${bid} invite`);
+
+    // Get the list of device notification tokens.
+    const getDeviceTokensPromise = admin.database().ref(`/student/${uid}/FCM Token`).once('value');
+
+    // Get liked profile
+    const getLikedProfilePromise = admin.auth().getUser(bid);
+
+    return Promise.all([getDeviceTokensPromise, getLikedProfilePromise]).then((results) => {
+        const tokensSnapshot = results[0];
+        const liker = results[1];
+
+        // Check if there are any device tokens.
+        if (!tokensSnapshot.hasChildren()) {
+            return console.log('There are no notification tokens to send to.');
+        }
+
+        console.log('There are', tokensSnapshot.numChildren(), 'tokens to send notifications to.');
+        console.log('Fetched liker profile', liker);
+
+        // Notification details
+        const payload = {
+            notification: {
+
+                body: 'A student has responsed to your invite. Go have a look!',
+                badge: '1',
+
+            },
+
+        };
+
+        // Listing all tokens.
+        const tokens = Object.keys(tokensSnapshot.val());
+
+        // Send notifications to all tokens.
+        return admin.messaging().sendToDevice(tokens, payload);
+
+    }).then((response) => {
+        // For each message check if there was an error.
+        const tokensToRemove = [];
+        response.results.forEach((results, index) => {
+            const error = result.error;
+            if (error) {
+                console.error('Failure sending notification to', tokens[index], error);
+                // Cleanup the tokens who are not registered anymore
+                if (error.code === 'messaging/invalid-registration-token' || error.code === 'messaging/registration-token-not-valid') {
+                    tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
+                }
+            }
+        });
+        return Promise.all(tokensToRemove)
+    });
+
+});
+
+
+
 exports.sendLikedNotification = functions.database.ref('/businessesLiked/{bid}/{uid}').onWrite((event) => {
 
     const uid = event.params.uid;
@@ -187,7 +249,7 @@ exports.sendMatchedNotification = functions.database.ref('/matches/{uid}/{bid}')
 });
 
 
-exports.sendInviteNotification = functions.database.ref('/organisedChats/{uid}/{bid}').onWrite((event) => {
+exports.sendInviteNotification = functions.database.ref('/organisedChats/{uid}/{bid}').onCreate((event) => {
 
     const uid = event.params.uid;
     const bid = event.params.bid;
@@ -216,7 +278,7 @@ exports.sendInviteNotification = functions.database.ref('/organisedChats/{uid}/{
         const payload = {
             notification: {
 
-                body: 'Congratulations! You have a new interivew invite',
+                body: 'Congratulations! You have a new interview invite',
                 badge: '1',
 
             },
